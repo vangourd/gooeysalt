1ed6696df7f30bfecabefe82917f8479205a4411
Brian Logan <mail@brianjlogan.com>
2018-08-27 19:08:06 -0400

    Added local storage caching

--- src/components/MinionList.vue
+++ src/components/MinionList.vue
@@ -1,12 +1,16 @@
 <template>
 <b-col>
     <h2>Minions</h2>
+    <div v-if="minions" v-for="minion in minions" :key="minion">
+        <b-card>{{ minion }}</b-card>
+    </div>
     <spinner v-if="!minions"></spinner>
 </b-col>
 
 </template>
 
 <script>
+import axios from 'axios'
 import Spinner from './Spinner.vue'
 
 export default {
@@ -19,9 +23,10 @@ export default {
                 if(this.state.auth.status == true){ 
                 axios.post('https://' + this.state.auth.server + 
                     ':' + this.state.auth.port + '/', {
-                        client: "local",
-                        tgt: '*',
-                        fun: "grains.items",
+                        client: "local_async",
+                        tgt: "*",
+                        fun: "test.arg",
+                        arg: "test"
                     },{
                     headers: {
                         'x-auth-token': this.state.auth.token,
@@ -29,19 +34,35 @@ export default {
                         'accept': 'application/json'
                     }
                           })
-                     .then((response) => this.jobs = response.data.return[0] )
+                     .then((response) => {
+                         this.jid = response['data']['return'][0].jid
+                         this.minions = response['data']['return'][0].minions
+                         localStorage.setItem('minions', JSON.stringify(response['data']['return'][0].minions))
+                         } )
                      .catch((error) => {
                         console.error(error)
                     })
                 }
             },
+        initView: function() {
+            console.debug('initView started')
+            if(localStorage.getItem('minions')){
+                console.debug('loading local storage')
+                this.minions = JSON.parse(localStorage.getItem('minions'))
+            }
+        }
     },
-    data: {
-        minions: null
+    data() {
+        return {
+           state: this.$root.sharedState.state,
+           minions: null,
+           jid: null
+        }
     },
     created() {
+        this.initView()
         this.loadMinions()
-        this.timer = setInterval(this.loadMinions, 10000)
+        this.timer = setInterval(this.loadMinions, 50000)
     }
 }
 </script>
