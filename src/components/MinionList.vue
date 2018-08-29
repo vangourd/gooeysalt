762c2dd38c49bc09707bc1f091d6201929d3561a
Brian Logan <blogan@blogan.sfb.osaa.net>
2018-08-29 11:09:03 -0400

    Working list of minions

--- src/components/MinionList.vue
+++ src/components/MinionList.vue
@@ -1,8 +1,16 @@
 <template>
 <b-col>
     <h2>Minions</h2>
-    <div v-if="minions" v-for="minion in minions" :key="minion">
-        <b-card>{{ minion }}</b-card>
+    <div v-if="minions" v-for="(value, key) in minions" :key="value.id">
+        <b-card v-if="value">
+            <i class="fab" :class="{ 'fa-windows large' : (value.kernel == 'Windows') }"></i>
+            <i class="fab" :class="{ 'fa-linux large' : (value.kernel == 'Linux') }"></i>
+            <strong>{{key}}</strong>
+        </b-card>
+        <b-card v-if="!value">
+            <i class="fa fa-bed"></i>
+            <strong>{{ key }}</strong>
+        </b-card>
     </div>
     <spinner v-if="!minions"></spinner>
 </b-col>
@@ -23,10 +31,9 @@ export default {
                 if(this.state.auth.status == true){ 
                 axios.post('https://' + this.state.auth.server + 
                     ':' + this.state.auth.port + '/', {
-                        client: "local_async",
+                        client: "local",
                         tgt: "*",
-                        fun: "test.arg",
-                        arg: "test"
+                        fun: "grains.items"
                     },{
                     headers: {
                         'x-auth-token': this.state.auth.token,
@@ -35,9 +42,11 @@ export default {
                     }
                           })
                      .then((response) => {
-                         this.jid = response['data']['return'][0].jid
-                         this.minions = response['data']['return'][0].minions
-                         localStorage.setItem('minions', JSON.stringify(response['data']['return'][0].minions))
+                         console.debug("Processing minions response")
+                         if(response) {
+                            this.minions = response['data']['return'][0]
+                            localStorage.setItem('minions', JSON.stringify(response['data']['return'][0]))
+                         }
                          } )
                      .catch((error) => {
                         console.error(error)
@@ -56,7 +65,7 @@ export default {
         return {
            state: this.$root.sharedState.state,
            minions: null,
-           jid: null
+           jid: null,
         }
     },
     created() {
