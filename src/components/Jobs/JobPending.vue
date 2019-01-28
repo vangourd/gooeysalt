668101f37454e6a416f5c289ac6aacc683b7de75
Brian Logan <mail@brianjlogan.com>
2019-01-28 18:28:18 -0500

    Cleaned up JobPending unnecessary api query function

--- src/components/Jobs/JobPending.vue
+++ src/components/Jobs/JobPending.vue
@@ -29,34 +29,7 @@ export default {
         return {
             state: this.$root.sharedState.state,
             apiTime: Date.now(),
-            jobQuery: null
         }
-    },
-    methods: {
-        queryProgress: function() {
-            axios.post('https://' + this.state.auth.server + 
-                ':' + this.state.auth.port + '/', {
-                    client: "runner",
-                    fun: "jobs.lookup_jid",
-                    arg: this.jid
-                },{
-                headers: {
-                    'x-auth-token': this.state.auth.token,
-                    'content-type': 'application/json',
-                    'accept': 'application/json'
-                }
-                        })
-                    .then((response) => {
-                        this.jobQuery = response
-                       console.debug('From jobpending ' + response)
-                    })
-                    .catch((error) => {
-                        console.debug(error)       
-                    })
-        }
-    },
-    created(){
-        this.queryProgress()
     },
     computed: {
         TotalTaskedMinions: function() {
