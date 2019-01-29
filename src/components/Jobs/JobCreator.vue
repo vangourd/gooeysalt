64d2be5b48d91ac8e02fe55909f2c159161c3135
Brian Logan <mail@brianjlogan.com>
2019-01-29 17:35:26 -0500

    Created visual break between Command reference and Tutorial

--- src/components/Jobs/JobCreator.vue
+++ src/components/Jobs/JobCreator.vue
@@ -8,8 +8,8 @@
         </b-btn>
         <b-collapse id="collapseCreateJob">
             <strong>Create new job</strong><br>
-            <a target="_blank" href="https://docs.saltstack.com/en/latest/topics/execution/remote_execution.html">Tutorial </a>
-            <a target="_blank" href="https://docs.saltstack.com/en/latest/ref/modules/all/">List of commands</a>
+            <a target="_blank" href="https://docs.saltstack.com/en/latest/topics/execution/remote_execution.html">Tutorial </a> <br> 
+            <a target="_blank" href="https://docs.saltstack.com/en/latest/ref/modules/all/">Command Reference</a>
             <b-form-group>
                 <b-form-input v-model="jobInput" placeholder="Enter your command here"></b-form-input>
                 <b-form-text v-if="stagedJob.data.client">Type: {{ stagedJob.data.client}}</b-form-text>
