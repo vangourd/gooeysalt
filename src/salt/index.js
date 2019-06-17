5fa0ef5a78dc40b59e9adb1684cb5a23adf489fc
Brian Logan <blogan@osaa.net>
2019-06-17 10:52:03 -0400

    Removed legacy ES6 Module code for app logic.

--- src/salt/index.js
+++
@@ -1,7 +0,0 @@
-import Auth from './Auth.js'
-import {
-    MinionsHandler,
-    ActiveJobsHandler,
-    CompleteJobsHandler,StatesHandler} from './Queries.js'
-
-export {Auth, MinionsHandler, ActiveJobsHandler, CompleteJobsHandler, StatesHandler}
